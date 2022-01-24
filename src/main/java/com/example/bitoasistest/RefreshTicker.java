e88436bdc412725bd4e289857ec1955278f50a63
mohammadmiyan <mohammad.miyan@intigral.net>
2022-01-24 10:52:55 +0400

    exception added

--- src/main/java/com/example/bitoasistest/RefreshTicker.java
+++ src/main/java/com/example/bitoasistest/RefreshTicker.java
@@ -3,13 +3,15 @@ package com.example.bitoasistest;
 import com.example.bitoasistest.client.TickerClient;
 import com.example.bitoasistest.pojo.Ticker;
 import com.example.bitoasistest.repo.TickerRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RefreshTicker {
-
+    private static final Logger log = LogManager.getLogger(RefreshTicker.class);
     private TickerRepository tickerRepository;
     private TickerClient tickerClient;
 
@@ -21,7 +23,12 @@ public class RefreshTicker {
     @Async
     @Scheduled(fixedRate = 10000)
     public void reportCurrentTime() throws InterruptedException {
-        Ticker tickerData = tickerClient.fetchTickerData();
-        tickerRepository.save(tickerData);
+        try {
+            Ticker tickerData = tickerClient.fetchTickerData();
+            tickerRepository.save(tickerData);
+        } catch (Exception ex) {
+            log.error("Error fetchTickerData {}", ex);
+        }
+
     }
 }
